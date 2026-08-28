3bdb932f612f78d4ea327586cce9ea7b975446cf
SyyedMateen <syyedmaten@gmail.com>
2026-08-28 22:29:11 +0500

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -39,7 +39,7 @@ export default function Nav() {
           }}
         >
           <a
-            href="#Syyedmateen"
+            href="#top"
             className="nav__brand"
             aria-label={`${nav.name} home`}
           >
